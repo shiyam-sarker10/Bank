b4f6949235304f11387fc1b3c25fcb5e0d57bebb
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-08-15 22:16:47 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const email = document.getElementById('email')
+const password = document.getElementById('pass')
+const btn = document.getElementById('submit-btn')
+const error = document.getElementById('error')
+const error2 = document.getElementById('error2')
+
+btn.addEventListener('click',function(){
+    const realPassword = "1234";
+    const realEmail = "shiyamsarker13@gmail.com";
+    const emailValue = email.value ;
+    const passValue = pass.value;
+    if(emailValue === "" || passValue === ""){
+        error2.style.display="inline"
+    }
+    else if(emailValue !== realEmail || passValue !== realPassword){
+        error.style.display="inline"
+        error2.style.display="none"
+    }
+    else{
+
+    }
+})
